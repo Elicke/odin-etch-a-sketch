76a1a2f16861553e126d67d1be40f72687af98b1
Kyle Manning <mkylee2@gmail.com>
2024-08-28 01:54:38 -0700

    Add custom grid sizing

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,8 @@
 window.onload = () => {
-    let gridContainer = document.querySelector("#grid-container");
-    let gridSquare = document.createElement("div");
-    gridSquare.className = "grid-square";
+    const gridContainer = document.querySelector("#grid-container");
+    const newGridButton = document.querySelector("#new-grid-btn");
 
-    for (let i = 1; i <= (16 ** 2); i++) {
-        let gridSquareClone = gridSquare.cloneNode();
-        gridContainer.appendChild(gridSquareClone);
-    }
+    createGrid(16, gridContainer);
 
     gridContainer.addEventListener("mouseover", (e) => {
         let target = e.target;
@@ -15,13 +11,27 @@ window.onload = () => {
         }
     });
 
-    let newGridButton = document.querySelector("#new-grid-btn");
-
     newGridButton.addEventListener("click", (e) => {
         let newSize;
         do {
             newSize = prompt("Enter a value 1-100 for the number of squares per side for the new grid:");
             newSize = parseInt(newSize);
         } while (isNaN(newSize) || newSize < 1 || newSize > 100);
+        createGrid(newSize, gridContainer);
     });
+}
+
+function createGrid(gridSize, gridContainer) {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    let gridSquare = document.createElement("div");
+    gridSquare.className = "grid-square";
+    gridSquare.style.flex = `1 1 ${100 / gridSize}%`;
+
+    for (let i = 1; i <= (gridSize ** 2); i++) {
+        let gridSquareClone = gridSquare.cloneNode();
+        gridContainer.appendChild(gridSquareClone);
+    }
 }
